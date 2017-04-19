b0f2f5617d9a00cf38bb4295d21a52309756c036
Luke Holman <lukeholman@gmail.com>
2017-04-19 10:33:46 +1000

    New data with correct n

--- web_app_data_format.py
+++ web_app_data_format.py
@@ -1,16 +1,28 @@
 import json
 
-inFileName = 'data_for_web_app_unparsed.json'
+## File name of file to be re-formatted
+inFileName = 'data_for_web_app.json'
 
-outFileName = 'data_for_web_app_no_list_no_dup_disc.json'
+## File name of the new file to be generated and saved
+outFileName = 'data_for_web_app_no_list.json'
 
+# load file
 with open(inFileName, 'r') as f:
     dat = json.load(f)
 
+# for each data set in the file
 for i in range(len(dat)):
+
+	# for each attribute or bit of data
     for k in dat[i].keys():
-        if len(dat[i][k]) == 1 and type(dat[i][k])==list:
+
+    	# if data is a list and has only one thing
+        if len(dat[i][k])==1 and type(dat[i][k])==list:
+
+        	# get rid of the list and make it the one thing
             dat[i][k] = dat[i][k][0]
 
+
+## Save new json
 with open(outFileName, 'w') as f:
     json.dump(dat, f)
